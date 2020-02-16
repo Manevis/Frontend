3c0cba5c4032cd152e4f17af8bb98d6470536f61
Moein Alizadeh <truemoein@gmail.com>
2020-02-17 01:44:28 +0330

    Tweak.

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import 'bootstrap/scss/bootstrap-reboot.scss'
+import 'bootstrap/scss/bootstrap-grid.scss'
 import '../assets/style/styles.scss'
 
 // This default export is required in a new `pages/_app.js` file.
